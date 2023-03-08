3ce576c94be1413a2e962e92402e84406d897dac
dlawle <daniel.lawler@outlook.com>
2023-03-08 00:34:12 -0500

    updating plinks
    adding CV in and envelope processing

--- Plinks/Plinks.cpp
+++ Plinks/Plinks.cpp
@@ -1,52 +1,65 @@
+#include "daisy_bed.h"
 #include "daisy_patch_sm.h"
 #include "daisysp.h"
+#include "arp_notes.h"
 
 using namespace daisy;
 using namespace daisysp;
 using namespace patch_sm;
+using namespace arps;
 
 DaisyPatchSM		hw;
 PolyPluck<32>		pp;
-ReverbSc			rv;
+ReverbSc 			rv;
 MoogLadder			ladder;
-Metro				tick;
+AdEnv      			env;
 
-// MIDI note numbers for a major triad
-const float kArpeggio[3] = {48.0f, 52.0f, 55.0f};
 uint8_t     arp_idx;
+uint8_t 	scale_idx;
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
 	hw.ProcessAllControls();
-
-	float trig, freq, sig_out;
+	float freq, nn, trig, sig_out;
 	trig = 0.f; 
 
-	float speed = fmap(hw.adc.GetFloat(ADC_12), 1.f, 40.f);
-	tick.SetFreq(speed);
-
-	float decay = fmap(hw.adc.GetFloat(CV_4), 0.f, 1.f);
+	float decay = fmap(hw.GetAdcValue(CV_4), 0.f, 1.f);
 	pp.SetDecay(decay);
 	
-	float feedback = fmap(hw.adc.GetFloat(CV_3), 0.f, 1.f);
+	float feedback = fmap(hw.GetAdcValue(CV_3), 0.f, 1.f);
 	rv.SetFeedback(feedback);
 
-	float lpf = fmap(hw.adc.GetFloat(CV_2), 1000.f, 32000.f, Mapping::LOG);
+	float lpf = fmap(hw.GetAdcValue(CV_2), 1000.f, 32000.f, Mapping::LOG);
 	rv.SetLpFreq(lpf);
 
-	float mlpf = fmap(hw.adc.GetFloat(CV_1), 1000.f, 32000.f, Mapping::LOG);
+	float mlpf = fmap(hw.GetAdcValue(CV_1), 1000.f, 32000.f, Mapping::LOG);
 	ladder.SetFreq(mlpf);
+
+	float attack = fmap(hw.GetAdcValue(ADC_12), 0.1f, 5.0f);
+	float release = fmap(hw.GetAdcValue(ADC_11), 0.1f, 5.0f);
+	env.SetTime(ADENV_SEG_ATTACK, attack);
+	env.SetTime(ADENV_SEG_DECAY, release);
+
+	// Set MIDI Note for new Pluck notes.
+    nn = 24.0f + hw.GetAdcValue(CV_8) * 60.0f;
+    nn = static_cast<int32_t>(nn); // Quantize to semitones
 
 	for (size_t i = 0; i < size; i++)
 	{
-		if(tick.Process()){
-			arp_idx = (arp_idx + 1) % 3; // advance the kArpeggio, wrapping at the end.
+		if(hw.gate_in_1.Trig()){
+			arp_idx = (arp_idx + 1) % 5; // advance the kArpeggio, wrapping at the end.
 			trig = 1.f;
 		}
-		freq    =  kArpeggio[arp_idx];
+
+		if(hw.gate_in_2.Trig()){
+			env.Trigger();
+		}
+
+		freq = nn + noMajFif[arp_idx];
 
 		sig_out = pp.Process(trig, freq);
 		sig_out = ladder.Process(sig_out);
+		sig_out *= env.Process();
 
 		rv.Process(sig_out, sig_out, &OUT_L[i], &OUT_R[i]);
 
@@ -61,6 +74,9 @@ int main(void)
 	hw.SetAudioBlockSize(4); // number of samples handled per callback
 	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
 	float sample_rate = hw.AudioSampleRate();
+
+	// init board
+	InitBed();
 	
 	// pluck init 
 	pp.Init(sample_rate);
@@ -72,9 +88,14 @@ int main(void)
     ladder.Init(sample_rate);
     ladder.SetRes(0.4);
 
-	// metro init 
-	tick.Init(5.f, sample_rate);
+	// envelope
+	env.Init(sample_rate);
+	env.SetMin(0.0);
+    env.SetMax(0.25);
 
 	hw.StartAudio(AudioCallback);
-	while(1) {}
+	while(1) {
+		bool state = hw.gate_in_1.State();
+		gate_in_led.Write(state);
+	}
 }
---
+++ Plinks/arp_notes.h
@@ -0,0 +1,27 @@
+namespace arps {
+// direction
+enum direction {
+    up = 0,
+    down,
+    upUp,
+    downDown,
+    upDown,
+    downUp,
+    random,
+    LAST_DIR
+};
+// 0 scale
+const float noMajTri[3] = {0, 4, 7};
+const float noMinTri[3] = {0, 3, 7};
+const float noMajFif[5] = {0, 2, 4, 5, 7};
+const float noMinFif[5] = {0, 2, 3, 5, 7};
+
+// triples
+//const float majTri[3] = {48.0f, 52.0f, 55.0f};
+//const float minTri[3] = {48.0f, 51.0f, 55.0f};
+//
+// fifths
+//const float majFif[5] = {48.0f, 50.0f, 52.0f, 53.0f, 55.0f};
+//const float minFif[5] = {48.0f, 50.0f, 51.0f, 53.0f, 55.0f};
+
+} // namespace arps
